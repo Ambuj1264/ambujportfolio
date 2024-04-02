4dabfe0c40db22295923c67675b5537572a98695
ambuj <ambuj1264675@gmail.com>
2024-04-03 03:14:02 +0530

    changes

---
+++ src/components/cover/ProfilePic.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import { FC } from 'react';
+
+interface ProfilePicProps {}
+
+const ProfilePic: FC<ProfilePicProps> = ({}) => {
+    return (
+        <div className="">
+            <div className="h-24 w-24 relative">
+                <Image
+                    alt="Ambuj Singh"
+                    src="/images/IMG_20220415_110542.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ProfilePic;
